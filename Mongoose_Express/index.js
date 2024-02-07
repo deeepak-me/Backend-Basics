b5a68d1610cc3d96c75ad43189a83688b4bf2c3e
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-07 16:51:05 +0530

    feat : Product model created

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+
+const Product = require("./models/product");
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/shopApp")
+  .connect("mongodb://127.0.0.1:27017/farmStand")
   .then(() => {
     console.log("MONGO CONNECTION OPEN");
   })
