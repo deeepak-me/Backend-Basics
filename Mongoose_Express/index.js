b61d7c06f5ca55a2db8f7fa852ff01b3e690c1dd
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-08 01:46:37 +0530

    feat: routes for to get route form and put updated content

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const Product = require("./models/product");
 
@@ -18,6 +19,7 @@ mongoose
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
@@ -40,6 +42,29 @@ app.get("/products/:id", async (req, res) => {
   const product = await Product.findById(id);
   console.log(product);
   res.render("products/show", { product });
+});
+
+//TO UPDATE THE PRODUCT
+
+//to get edit form
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+//to get edited product
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+  // console.log(req.body);
+  // res.send("PUT!!");
 });
 
 app.listen(3000, () => {
