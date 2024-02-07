5b88b6376d4a708c913ae25228a40e7515196471
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-07 20:55:57 +0530

    feat: single product details route created

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -18,9 +18,16 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/Products", async (req, res) => {
+app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  console.log(product);
+  res.render("products/show", { product });
 });
 
 app.listen(3000, () => {
