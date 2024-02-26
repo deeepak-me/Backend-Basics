ed01daafcbaf0f5dee59056b19dc371a2538f95b
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-26 23:33:08 +0530

    feat : farm routes created

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -5,9 +5,10 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 
 const Product = require("./models/product");
+const Farm = require("./models/farm");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/farmStand")
+  .connect("mongodb://127.0.0.1:27017/farmStandTake2")
   .then(() => {
     console.log("MONGO CONNECTION OPEN");
   })
@@ -21,6 +22,54 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 
+// FARM ROUTES
+
+app.get("/farms", async (req, res) => {
+  const farms = await Farm.find({});
+  res.render("farms/index", { farms });
+});
+
+app.get("/farms/new", (req, res) => {
+  res.render("farms/new");
+});
+
+app.get("/farms/:id", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id).populate("products");
+  res.render("farms/show", { farm });
+});
+
+app.delete("/farms/:id", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findByIdAndDelete(id);
+  res.redirect("/farms");
+});
+
+app.post("/farms", async (req, res) => {
+  const farm = new Farm(req.body);
+  await farm.save();
+  res.redirect("/farms");
+});
+
+app.get("/farms/:id/products/new", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  res.render("products/new", { categories, farm });
+});
+
+app.post("/farms/:id/products", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  const { name, price, category } = req.body;
+  const product = new Product({ name, price, category });
+  farm.products.push(product);
+  product.farm = farm;
+  await farm.save();
+  await product.save();
+  res.redirect(`/farms/${id}`);
+});
+
+//PRODUCT ROUTES
 const categories = ["fruit", "vegetable", "dairy"];
 
 app.get("/products", async (req, res) => {
@@ -41,13 +90,13 @@ app.get("/products/new", (req, res) => {
 app.post("/products", async (req, res) => {
   const newProduct = new Product(req.body);
   await newProduct.save();
-  console.log(newProduct);
+  //console.log(newProduct);
   res.redirect(`/products/${newProduct._id}`);
 });
 
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findById(id);
+  const product = await Product.findById(id).populate("farm", "name");
   console.log(product);
   res.render("products/show", { product });
 });
