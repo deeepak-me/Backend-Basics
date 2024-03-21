fc65d5b322cfe917cdc9f6001001cac22de2ed2b
deeepak-me <deepak.krishnan.e@gmail.com>
2024-03-21 22:24:44 +0530

    feat: Logout implemented

--- authDemo/index.js
+++ authDemo/index.js
@@ -52,11 +52,17 @@ app.post("/login", async (req, res) => {
   }
 });
 
+app.post("/logout", (req, res) => {
+  //req.session.user_id = null;
+  req.session.destroy();
+  res.redirect("/login");
+});
+
 app.get("/secret", (req, res) => {
   if (!req.session.user_id) {
-    res.redirect("/login");
+    return res.redirect("/login");
   }
-  res.send("THIS IS SECRET! YOU CANNOT SEE THIS UNTIL YOU GET LOGGED IN");
+  res.render("secret");
 });
 app.listen(3030, () => {
   console.log("SERVING ON PORT 3030");
